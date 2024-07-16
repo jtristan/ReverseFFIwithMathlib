128030c27ced01b2561924df30be4edcacc236e1
jtristan <trjohnb@amazon.com>
2024-07-16 18:38:47 -0400

    upgrade

--- ReverseFFIWithMathlib.lean
+++ ReverseFFIWithMathlib.lean
@@ -1,6 +1,5 @@
-import Std.Data.RBMap.Basic
+import Batteries.Data.RBMap.Basic
 
 @[export min_number]
-def minNumber (x y z : USize) : Float := 
-  let map : Std.RBSet USize compare := Std.RBSet.ofList [x,y,z] _
-  (map.toList.head!).toUInt64.toFloat
+def minNumber (x y z : USize) : Float :=
+  if x < y then 4.5 else if y < z then 3.2 else 6.7
--- lakefile.lean
+++ lakefile.lean
@@ -11,14 +11,13 @@ lean_lib «ReverseFFIWithMathlib» where
 lean_lib «reverseffiwithmathlib» where
   roots := #[`ReverseFFIWithMathlib]
   defaultFacets := #[`shared]
-  moreLinkArgs := 
+  moreLinkArgs :=
     #["-L.lake/packages/aesop/.lake/build/lib/", "-lAesop",
       "-L.lake/packages/Cli/.lake/build/lib/", "-lCli",
       "-L.lake/packages/importGraph/.lake/build/lib/", "-lImportGraph",
       "-L.lake/packages/mathlib/.lake/build/lib/", "-lMathlib",
       "-L.lake/packages/proofwidgets/.lake/build/lib/", "-lProofWidgets",
       "-L.lake/packages/Qq/.lake/build/lib/", "-lQq",
-      "-L.lake/packages/std/.lake/build/lib/", "-lStd",
       "-lLake"]
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
